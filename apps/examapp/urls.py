4e1c2bb2bdfecea9936bfb3b4ec2d79f77908312
Your Name <cjwmay@live.cn>
2017-04-27 20:21:19 -0700

    tryagain

--- apps/examapp/urls.py
+++ apps/examapp/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views           # So we can call functions from our routes!
 urlpatterns = [
 	url(r'^main$', views.index),
-    	url(r'^checkregister$', views.checkregister),
-    	url(r'^checklogin$', views.checklogin),
+    url(r'^checkregister$', views.checkregister),
+    url(r'^checklogin$', views.checklogin),
 	url(r'^logout$', views.logout),
-    	url(r'^quotes$', views.success),
+    url(r'^quotes$', views.success),
 	url(r'^addquote$', views.addquote),
 	url(r'^addfav$', views.addfav),
 	url(r'^remove$', views.remove),
